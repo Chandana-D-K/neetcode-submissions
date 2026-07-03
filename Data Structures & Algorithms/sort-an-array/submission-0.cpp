ee78cdc5db5d31bdf0bb5df91da97699dc969b41
Chandana-D-K <chandanadk049@gmail.com>
2026-07-03 10:32:11 +0530

    Add: sort-an-array - submission-0

---
+++ Data Structures & Algorithms/sort-an-array/submission-0.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<int> sortArray(vector<int>& nums) {
+        unordered_map<int,int>mpp;
+        for(int &num:nums){
+            mpp[num]++;
+        }
+
+        int mini=*min_element(nums.begin(),nums.end());
+        int maxi=*max_element(nums.begin(),nums.end());
+
+        int i=0;
+
+        for(int num=mini;num<=maxi;num++){
+            while(mpp[num]>0){
+                nums[i]=num;
+                i++;
+                mpp[num]--;
+            }
+            
+        }
+
+        return nums;
+    }
+};
